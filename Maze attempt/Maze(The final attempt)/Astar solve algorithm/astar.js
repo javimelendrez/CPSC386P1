ede64c3735e9b79c5a5b7163424fe9ce67b89091
Yoshimitsu739 <armandoosegueda793@gmail.com>
2020-09-19 20:25:46 -0700

    Update astar.js

--- Maze attempt/Maze(The final attempt)/Astar solve algorithm/astar.js
+++ Maze attempt/Maze(The final attempt)/Astar solve algorithm/astar.js
@@ -180,3 +180,187 @@ for ( var i=0; i<path.length; i++){//blue
   path[i].show(color(0,0,255));
 }
 }
+
+/*possible astar function
+this.astarsolver = function(){
+    var cols=40;
+    var rows=40;
+    var grid= new Array(cols);//an array of columns
+
+    var openSet=[];
+    var closedSet=[];
+    var start;
+    var end;
+    var w,h;
+    var path = [];
+    function setup(){
+      console.log('A*');
+
+      w = width/cols;
+      h = height/rows;
+
+      //making a 2D array
+      for (var i=0; i < cols; i++){
+        grid[i] = new Array(rows);// creates an array of rows
+      }
+
+        for (var i=0; i < cols; i++){
+          for (var j=0; j < rows; j++){
+          grid[i][j] = new Spot(i, j);
+        }
+    }
+        for (var i=0; i < cols; i++){
+          for (var j=0; j < rows; j++){
+          grid[i][j].addNeighbors(grid);
+        }
+      }
+
+
+
+    start=grid[0][0];//start point to solve from
+    end=grid[cols-1][rows-1];// end point to get to
+
+    openSet.push(start);
+
+      console.log(grid);
+    }
+
+    function removal(arr, elt) {
+      for (var i = arr.length-1; i >=0; i--) {
+        if (arr[i]== elt) {
+          arr.splice(i, 1);
+        }
+      }
+    }
+
+    function heuristic(a, b){
+      //var d = dist(a.i,a.j,b.i,b.j) //euclidean distance
+      var d = abs(a.i-b.i) + abs(a.j-b.j) //matthattan distance
+      return d;
+    }
+
+    function Spot(i,j){
+      this.i=i;
+      this.j=j;
+      this.f=0;
+      this.g=0;
+      this.h=0;
+      this.neighbors=[];
+      this.previous = undefined;
+
+      this.show=function(col){
+        fill(col);
+        noStroke();
+        rect(this.i*w, this.j*h, w-1, h-1);
+      }
+      this.addNeighbors=function(grid){
+        var i = this.i;
+        var j = this.j;
+        if(i<cols-1){
+          this.neighbors.push(grid[i+1][j]);
+      }
+        if(i>0){
+      this.neighbors.push(grid[i-1][j]);
+    }
+        if(j<rows-1){
+      this.neighbors.push(grid[i][j+1]);
+    }
+        if(j>0){
+      this.neighbors.push(grid[i][j-1]);
+        }
+      }
+
+
+
+
+    }
+
+
+    // use the draw function to loop the algorithm
+    function draw(){
+
+    if (openSet.length>0){
+      var lowestIndex = 0;
+      for (var i = 0; i < openSet.length; i++){
+          if (openSet[i].f < openSet[lowestIndex].f){
+            lowestIndex = i;
+          }
+        }
+        var current = openSet[lowestIndex];
+
+        if (current === end){
+          console.log("DONE!");
+        }
+        removal(openSet,current);
+        closedSet.push(current);
+
+
+        var neighbors = current.neighbors;
+        for (var i = 0; i< neighbors.length; i++){
+            var neighbor = neighbors[i];
+
+            if(!closedSet.includes(neighbor)){
+                var tempG = current.g + 1;
+
+                if (openSet.includes(neighbor)){
+                  if (tempG < neighbor.g) {
+                    neighbor.g = tempG;
+                  }
+                } else {
+                  neighbor.g = tempG;
+                  openSet.push(neighbor);
+                }
+
+                neighbor.h = heuristic(neighbor, end);
+                neighbor.f = neighbor.g + neighbor.h;
+                neighbor.previous = current;
+
+
+              }
+
+        }
+
+
+
+
+    //we keep going
+    } else {
+
+        //no solution
+
+      }
+
+
+
+
+
+    for (var i=0; i<cols; i++){
+      for (var j=0; j<rows; j++){
+        grid[i][j].show(color(0));
+      }
+    }
+
+    for (var i=0; i < closedSet.length; i++){//red
+      closedSet[i].show(color(255,0,0));
+    }
+
+    for (var i=0; i < openSet.length; i++){//green
+      openSet[i].show(color(0,255,0));
+    }
+
+    //find the path
+    path = [];
+    var temp = current;
+    path.push(temp);
+    while(temp.previous){
+      path.push(temp.previous);
+      temp=temp.previous;
+    }
+
+    for ( var i=0; i<path.length; i++){//blue
+      path[i].show(color(0,0,255));
+    }
+    }
+
+  }
+*/
